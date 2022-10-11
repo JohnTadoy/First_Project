b6272307f584db97108f65907f3e7aecb9440f62
JohnTadoy <johntadoy1@gmail.com>
2022-10-11 10:31:03 -0500

    Added validation for inputting data into table

--- app/models/overwatch_character.rb
+++ app/models/overwatch_character.rb
@@ -1,3 +1,7 @@
 class OverwatchCharacter < ApplicationRecord
   belongs_to :overwatch_class
+  validates :name, presence: true
+  validates :difficulty, presence: true
+  validates :image, presence: true
+  validates :description, presence: true
 end
--- app/models/overwatch_class.rb
+++ app/models/overwatch_class.rb
@@ -1,3 +1,4 @@
 class OverwatchClass < ApplicationRecord
   has_many :overwatch_characters
+  validates :name, presence: true
 end

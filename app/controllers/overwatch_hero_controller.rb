242359820bb73799b69946f4beb27d4f94be10b9
JohnTadoy <johntadoy1@gmail.com>
2022-10-20 21:35:23 -0500

    added hierarchical collection navigation

--- app/controllers/overwatch_hero_controller.rb
+++ app/controllers/overwatch_hero_controller.rb
@@ -8,15 +8,13 @@ class OverwatchHeroController < ApplicationController
   end
 
   def search
-    search = "%#{params[:name]}%"
+    search = "%#{params[:q]}%"
     hero = "%#{params[:class]}%"
 
     if hero == ""
       @hero = OverwatchCharacter.where("name LIKE ?", search)
-      @hero_pagination = OverwatchCharacter.where("name LIKE ?", wildcard_search).page params[:page]
     else
-      @hero = OverwatchCharacter.where("name LIKE ? AND overwatch_class_id = ?", search, hero)
-      @hero_pagination = OverwatchCharacter.where("name LIKE ? AND overwatch_class_id = ?", search, hero).page params[:page]
+      @hero = OverwatchCharacter.where("name LIKE ? AND overwatch_class_id LIKE ?", search, hero)
     end
   end
 end
